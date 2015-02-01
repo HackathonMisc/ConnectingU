631959a5b9274fc8bebc0f4934faa3a37cc6ff9c
eyesniper2 <eyesniper2@rayfall.net>
2015-01-31 20:12:45 -0700

    Add Start of listView

--- ConnectingU/src/com/example/connectingu/Buy.java
+++ ConnectingU/src/com/example/connectingu/Buy.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Buy extends ActionBarActivity {
 
@@ -11,6 +13,26 @@ public class Buy extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_buy);
+		
+		final ListView listView = (ListView) findViewById(R.id.list);
+		String[] values = new String[] { "Android List View", 
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android", 
+                "Android Example", 
+                "List View Source Code", 
+                "List View Array Adapter", 
+                "Android Example List View" 
+               };
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+	              android.R.layout.simple_list_item_1, android.R.id.text1, values);
+	    
+	    
+	            listView.setAdapter(adapter); 
+	            
+	           
+		
+		
 	}
 
 	@Override
